2b489c5b059c3c21e69633c4bca82a691be0ae5a
Hanz Po <hanznathanpo@gmail.com>
2023-08-14 02:05:52 -0400

    Complete exercise 1.6

--- part1/practice/src/App.jsx
+++ part1/practice/src/App.jsx
@@ -1,17 +1,45 @@
 import { useState } from "react";
 
+const History = ({ allClicks }) => {
+  if (allClicks.length == 0) {
+    return <p>Click on a button to get started</p>;
+  }
+
+  return <p>{allClicks.join(", ")}</p>;
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
 
   return (
     <div>
-      <button onClick={() => setCounter(counter + 1)}>
-        The current count is: {counter}
-      </button>
-      <br />
-      <button onClick={() => setCounter(0)}>
-        Reset the count
-      </button>
+      {left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
+      {total}
     </div>
   );
 };
