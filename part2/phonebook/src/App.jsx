cc8056d2a0d3cabd9f6c66640e106cc2a3f6e54d
Hanz Po <hanznathanpo@gmail.com>
2023-08-17 01:44:06 -0400

    Complete exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,49 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
   const addPerson = (event) => {
     event.preventDefault();
+    let exists = false;
 
     const personObject = {
       name: newName,
+      number: newNumber,
+      id: persons.length + 1,
     };
 
-    setPersons(persons.concat(personObject));
+    persons.forEach((person) => {
+      if (JSON.stringify(person.name) === JSON.stringify(personObject.name)) {
+        alert(`${newName} is already added to phonebook`);
+        exists = true;
+      }
+    });
+
     setNewName("");
+    setNewNumber("");
+
+    if (exists === true) {
+      return;
+    }
+
+    setPersons(persons.concat(personObject));
   };
 
   return (
@@ -27,12 +54,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <div key={person.name}>{person.name}</div>
+        <div key={person.id}>{person.name} {person.number}</div>
       ))}
     </div>
   );
