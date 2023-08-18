4f387b887b5d6e465f5cd9b5687387a3d8062a80
Hanz Po <hanznathanpo@gmail.com>
2023-08-18 11:51:57 -0400

    Update notes

--- part2/practice/src/App.jsx
+++ part2/practice/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+
+import noteService from "./services/notes";
+
 import Note from "./components/Note";
 
 const App = (props) => {
@@ -8,8 +11,8 @@ const App = (props) => {
   const [showAll, setShowAll] = useState(true);
 
   const hook = () => {
-    axios.get("http://localhost:3001/notes").then((response) => {
-      setNotes(response.data);
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
     });
   };
 
@@ -23,15 +26,31 @@ const App = (props) => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value);
+  };
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        alert(`the note '${note.content}' was already deleted from server`);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
   };
 
   return (
@@ -44,7 +63,11 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
