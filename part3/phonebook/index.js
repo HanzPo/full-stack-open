e5ddadb501eaa831fc58d87dcb7a0bc19284aa17
Hanz Po <hanznathanpo@gmail.com>
2023-09-04 17:31:52 -0400

    Complete exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -35,8 +35,23 @@ app.get("/info", (request, response) => {
   const options = {
     timeZoneName: "long"
   }
-
+  
   response.send(`Phonebook has info for ${persons.length} people<br/>${new Date(Date.now()).toLocaleString("en-CA", options)}`);
+});
+
+
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.statusMessage = "No Person";
+    response.status(404).end();
+  }
 });
 
 const PORT = 3002;
