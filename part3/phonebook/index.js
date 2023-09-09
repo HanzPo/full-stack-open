252c7b0f2698ffda265f4b7c0041709de7f56c20
Hanz Po <hanznathanpo@gmail.com>
2023-09-09 01:53:21 -0400

    Complete exercise 3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -116,7 +116,7 @@ app.delete("/api/persons/:id", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
