76e592daac1ee412a4b43fac0443f298f9ae060b
Hanz Po <hanznathanpo@gmail.com>
2023-09-12 17:56:13 -0400

    Update notes app up to next exercise

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+const Note = require("./models/note");
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
@@ -33,18 +37,20 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.statusMessage = "Cannot find this note";
-    response.status(404).end();
-  }
+  Note.findById(request.params.id)
+    .then((note) => {
+      response.json(note);
+    })
+    .catch((error) => {
+      response.statusMessage = "Cannot find this note";
+      response.status(404).end();
+    });
 });
 
 const generateId = () => {
@@ -61,15 +67,14 @@ app.post("/api/notes", (request, response) => {
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
