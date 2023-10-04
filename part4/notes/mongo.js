043cc8c2a595d6584f3eb619518b97d53dac7208
Hanz Po <hanznathanpo@gmail.com>
2023-10-04 18:46:56 -0400

    Practiced vim

--- part4/notes/mongo.js
+++ part4/notes/mongo.js
@@ -29,8 +29,8 @@ const Note = mongoose.model("Note", noteSchema);
 //   mongoose.connection.close();
 // });
 
-Note.find({}).then(result => {
-  result.forEach(note => {
+Note.find({}).then((result) => {
+  result.forEach((note) => {
     console.log(note);
   });
   mongoose.connection.close();
